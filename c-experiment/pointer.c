6fe8cfeeba9343052a6d26de113a7c2e6f831552
yashwantbr <yashwanthbr810@gmail.com>
2023-04-30 09:44:07 +0530

    double pointer updated

--- c-experiment/pointer.c
+++ c-experiment/pointer.c
@@ -4,14 +4,17 @@ int main(void)
 {
     int Var_1;
     int Var_2;
-    int *Ptr;
+    int *Ptr1;
+    int **Ptr2;
 
     Var_1 = 10;
-    Ptr = &Var_1; 
-    *Ptr = 20; //changing value of a pointer
-    Var_2 = *Ptr; //If a variable having indirection operation (*Ptr) will holds the value and address (&) will holds the address of variable
 
-    printf("value of Var_1 = %d, *Ptr = %d, Var_2 = %d,", Var_1, *Ptr, Var_2);
+    Ptr1 = &Var_1;
+    Ptr2 = &Ptr1;
+    *Ptr1 = 20; //changing value of a pointer
+    Var_2 = *Ptr1; //If a variable having indirection operation (*Ptr1) will holds the value and address (&) will holds the address of variable
+
+    printf("value of Var_1 = %d\n *Ptr1 = %d\n *Ptr1 = %d\n Var_2 = %d\n  **Ptr2 = %d\n ", Var_1, *Ptr1, Ptr1, Var_2, **Ptr2);
 
     return 0;
     
